db50de2ddbc70f34292785e90aee02d96a96692f
amir ahmadi <amirahmadidev@gmail.com>
2024-01-02 00:04:01 +0330

    add grpc server to auth service

--- auth-service/src/server.ts
+++ auth-service/src/server.ts
@@ -1,7 +1,10 @@
 // import { Channel } from "amqplib";
 // import { newConnection } from "@notifications/broker";
 
+import { createGrpcServer } from "./protos/server";
+
 export async function start() {
+    createGrpcServer()
     startQueues();
 }
 
